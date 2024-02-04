a3ae6238dd49544284a022bb6a0c4c3ea07db2ee
Ranajit <18ranajit2005@gmail.com>
2024-02-04 22:47:34 +0530

    ht

--- Exam/Q3Amean.py
+++ Exam/Q3Amean.py
@@ -1,7 +1,14 @@
 def mean(li):
     return sum(li)/len(li)
 
-
+def meadian(li):
+    Sortli = sorted(li)
+    n = len(li)
+    mid = n//2
+    if n%2 == 0:
+        return (Sortli[mid]+Sortli[mid-1])/2
+    else:
+        return Sortli[mid]
 
 # def main() :
 n = int(input("Give thye size of the array : "))
