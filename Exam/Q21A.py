1aed3b3268ec91884bae84fa8edcebc0bf7fb4a4
Ranajit <18ranajit2005@gmail.com>
2024-02-05 23:30:10 +0530

    kgwdjg

---
+++ Exam/Q21A.py
@@ -0,0 +1,12 @@
+def cude(num):
+    return num**3
+
+li = []
+n = int(input("Give the size : "))
+for i in range(0,n):
+    a = int(input("Give element : "))
+    li.append(a)
+list1 = list(map(cude,li))
+print(list1)
+print("Max : ",max(list1))
+print("Min : ",min(list1))
---
+++ Exam/Q21B.py
@@ -0,0 +1,6 @@
+n = int(input("Give the nth number : "))
+for i in range(0,n):
+    a = 2
+    b = 9
+    print((a+(i*3)),"/",(b+(i*4)),"+",end="")
+print("...")
